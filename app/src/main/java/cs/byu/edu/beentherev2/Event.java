784f7ce7a07ed5a3d150a19b061d75707066a736
Kade <kadegreenburg@gmail.com>
2022-04-04 08:41:06 -0600

    added Event and Journal classes for real this time

--- app/src/main/java/cs/byu/edu/beentherev2/Event.java
+++ app/src/main/java/cs/byu/edu/beentherev2/Event.java
@@ -1,2 +1,92 @@
-package cs.byu.edu.beentherev2;public class Event {
+package cs.byu.edu.beentherev2;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Event {
+    private String title;
+    private LatLng location;
+    private ArrayList<File> photos;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Float cost;
+    private ArrayList<String> tags;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
+
+    public ArrayList<File> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(ArrayList<File> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(File photo) {
+        this.photos.add(photo);
+    }
+
+    //TODO: implement a delete photo function? In case an invalid picture is chosen
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Float getCost() {
+        return cost;
+    }
+
+    public void setCost(Float cost) {
+        this.cost = cost;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
 }
--- app/src/main/java/cs/byu/edu/beentherev2/Journal.java
+++ app/src/main/java/cs/byu/edu/beentherev2/Journal.java
@@ -1,2 +1,70 @@
-package cs.byu.edu.beentherev2;public class Journal {
+package cs.byu.edu.beentherev2;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Journal {
+    private String title;
+    private File photo;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private ArrayList<Event> events;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public File getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(File photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(ArrayList<Event> events) {
+        this.events = events;
+    }
+
+    public void addEvent(Event event) {
+        this.events.add(event);
+    }
+
+    //TODO: implement method to delete an event
 }
