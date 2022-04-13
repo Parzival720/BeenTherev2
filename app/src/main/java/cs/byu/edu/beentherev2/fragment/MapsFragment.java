3caa21d0af51d7b66ae17b86bf6a359ad338866b
Kade <kadegreenburg@gmail.com>
2022-04-13 00:13:05 -0600

    added functionality for location button

--- app/src/main/java/cs/byu/edu/beentherev2/fragment/MapsFragment.java
+++ app/src/main/java/cs/byu/edu/beentherev2/fragment/MapsFragment.java
@@ -1,5 +1,8 @@
 package cs.byu.edu.beentherev2.fragment;
 
+import android.media.Image;
+import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -9,10 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -23,6 +23,7 @@ import cs.byu.edu.beentherev2.model.Journal;
 
 public class MapsFragment extends Fragment {
 
+    GoogleMap mMap;
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -36,6 +37,7 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
             MainActivity mainActivity = (MainActivity) getActivity();
 
             //grab all events and put their markers onto the map!
@@ -60,6 +62,20 @@ public class MapsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        //make location button take to current location (hardcoded to Provo right now)
+        ImageButton addButton = (ImageButton) view.findViewById(R.id.location_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CameraUpdate center =
+                        CameraUpdateFactory.newLatLng(new LatLng(40.24688, -111.64920));
+                CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+                mMap.moveCamera(center);
+                mMap.animateCamera(zoom);
+            }
+        });
+
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
